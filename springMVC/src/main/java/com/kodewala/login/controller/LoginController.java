26a9c272b6a903bf42542863f3fd90715aabe9b6
sagar <biswassagar4@gmail.com>
2026-01-27 21:42:31 +0530

    springMVC

---
+++ springMVC/src/main/java/com/kodewala/login/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.kodewala.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/login")
+	public ModelAndView login() {
+		System.out.println("LoginController.login()::::::::::::::::::::::::::::::::::::::::::::::::::::::;");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("displayLogin");
+		return mv;
+	}
+
+}
