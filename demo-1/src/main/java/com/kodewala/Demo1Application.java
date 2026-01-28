4fc465d329b7cd48b23286467030b0db782c589a
sagar <biswassagar4@gmail.com>
2026-01-29 00:40:41 +0530

    spring

---
+++ demo-1/src/main/java/com/kodewala/Demo1Application.java
@@ -0,0 +1,15 @@
+package com.kodewala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Demo1Application.main().........");
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
